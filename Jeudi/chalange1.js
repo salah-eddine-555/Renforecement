79dee0066d806f8d7a4096a15a56acfe027ea444
unknown <salah-eddine-555>
2026-03-29 23:32:25 +0200

    fair les 3 premier question poure le premier chalange de jeudi

---
+++ Jeudi/chalange1.js
@@ -0,0 +1,92 @@
+console.log('chalanged e jeudi  ========>');
+
+let reservations = [
+    {
+        id: 1,
+        nom_client: "Ahmed",
+        nombre_personnes: 4,
+        date: "2025-01-19",
+        heure: "2:00",
+        statut: "confirmée", 
+        telephone: "0612345678",
+    }
+]
+let count = 1;
+function reserver(nom_client, nbrPersonnes, date,heure, telephone){
+        let totalPersonnes = 0; 
+        for(let i =0; i < reservations.length; i++){
+            if(reservations[i].date === date && reservations[i].heure === heure){
+                totalPersonnes +=reservations[i].nombre_personnes;
+            }
+        }
+        if(totalPersonnes + nbrPersonnes > 30){
+            let NewReservation = {
+                'id': ++count,
+                'nom_client': nom_client,
+                'nombre_personnes': nbrPersonnes,
+                'date': date,
+                'heure': heure,
+                'statut': 'en attent',
+                'telephone': telephone
+            }
+
+            reservations.push(NewReservation);
+        }else{
+                let NewReservation = {
+                        'id': ++count,
+                       'nom_client': nom_client,
+                       'nombre_personnes': nbrPersonnes,
+                       'date': date,
+                       'heure': heure,
+                       'statut': 'confirme',
+                       'telephone': telephone,
+                       'tets': 'ahed',
+                   }
+               
+                   reservations.push(NewReservation);
+        }
+}
+
+function annulerReservation(reservationId){
+    
+    let reservation = reservations.find(r => r.id === reservationId);
+    
+    reservation.statut = 'annulee';
+
+    let totalRservationConfirmee = 0;
+    for(let i =0; i <reservations.length; i++){
+
+        if(reservations[i].date === reservation.date && reservations[i].heure === reservation.heure && reservations[i].statut === 'confirme'){
+            totalRservationConfirmee +=reservations[i].nombre_personnes;
+        }
+            
+    }
+
+    for(let res of reservations){
+        if(res.date == reservation.date && res.heure === reservation.heure && res.statut === 'en attent'){
+            if(totalRservationConfirmee + res.nombre_personnes <= 30){
+                res.statut = 'confirme';
+                totalRservationConfirmee += res.nombre_personnes;
+            }
+        }
+    }
+
+}
+
+function trParDate(date){res.sort((a, b) => a.heure - b.heure);
+
+    let res = reservations.filter(res => res.date === date);
+
+    res.sort((a, b) => a.heure - b.heure);
+}
+
+function CalculerTauxOccupation(){
+    
+}
+
+reserver('salah',26,'2025-01-19', '2:00','0624494495');
+reserver('ahmed',26,'2025-01-19', '2:00','0624494495');
+
+annulerReservation(2);
+
+console.log(reservations);
